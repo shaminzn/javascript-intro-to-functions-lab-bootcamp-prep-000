dab67ca6970eac6627317bceb9d7fbe45468cca4
Shamira Naznin <shaminzn@gmail.com>
2020-03-06 02:29:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var uppercase = "HELLO!"
 
-uppercase.toUpperCase() === uppercase
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase
-
-mixedCase.toUpperCase() === mixedCase
 
 function shout(string) {
   return string.toUpperCase();
@@ -20,21 +8,3 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function logShout(string) {
-  console.log(string.toUpperCase);
-}
-
-function logWhisper(string) {
-  console.log(string.toLowerCase);
-}
-
-function sayHiToGrandma(string) {
-
-  if (string.toLowerCase) {
-    return "I can\'t hear you!";
-  } else if (string.toUpperCase) {
-    return "YES INDEED!";
-  } else if (string === 'I love you, Grandma.') {
-    return "I love you, too."
-  }
-}
